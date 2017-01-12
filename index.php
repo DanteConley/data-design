0b802f5797e9fd1203ea05229111f5bb584e1434
Dante Conley <danteconley@icloud.com>
2017-01-12 09:46:49 -0700

    Added Use Case

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
 
 		<header>
 			<b><u>User Persona</u></b>
-		</header><br>
+		</header>
 
 		<ul>
 			<li><b>Name:</b> Dylan</li>
 			<li><b>Age:</b> 23</li>
 			<li><b>Occupation:</b> Music Producer and DJ</li>
-			<li><b>Technology:</b> Macbook Pro</li>
+			<li><b>Technology:</b> Macbook Pro macOS Sierra V 10.12.2</li>
 			<li><b>Needs:</b> Up and coming music producer using SoundCloud as a platform
 					to promote and share his music. He needs tools to see who is listening
 					and where they are listening from to better understand his audience.</li>
@@ -25,6 +25,26 @@
 				and keep them up to date on his new music.</li>
 		</ul>
 
+		<header>
+			<b><u>Use Case</u></b>
+		</header>
 
+		<p>
+			Dylan has just finished up a new "fire" EP. He needs to share it with his growing fan
+			base and also get his music to the ears of new possible fans. Dylan double checks that
+			he is happy with the mastered tracks on Ableton, he thinks they're "lit", so he exports
+			the files to his desktop as lossless .wav files. He proceeds to log into SoundCloud and
+			upload his new music along with some "trippy" album artwork his "homie" made. Knowing
+			his tunes are "Dank" he sits back and waits for the numerous e-mails filled with "mad
+			props" and booking requests to pour into his inbox. They do. And he is totally "stoked".
+		</p>
+
+		<header>
+			<b><u>Interaction Flow</u></b>
+		</header>
+
+		<p>
+
+		</p>
 	</body>
 </html>
