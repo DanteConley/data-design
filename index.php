355212df43c448ae8e948988aa61263f2722ba0c
Dante Conley <danteconley@icloud.com>
2017-01-12 10:16:45 -0700

    Added Interaction Flow

--- index.php
+++ index.php
@@ -16,10 +16,10 @@
 			<li><b>Name:</b> Dylan</li>
 			<li><b>Age:</b> 23</li>
 			<li><b>Occupation:</b> Music Producer and DJ</li>
-			<li><b>Technology:</b> Macbook Pro macOS Sierra V 10.12.2</li>
+			<li><b>Technology:</b> MacBook Pro macOS Sierra V 10.12.2</li>
 			<li><b>Needs:</b> Up and coming music producer using SoundCloud as a platform
-					to promote and share his music. He needs tools to see who is listening
-					and where they are listening from to better understand his audience.</li>
+				to promote and share his music. He needs tools to see who is listening
+				and where they are listening from to better understand his audience.</li>
 			<li><b>Goals:</b> He wants to find his niche market and make it big as a DJ.
 				Once fame is achieved SoundCloud will still be utilized to interact with fans
 				and keep them up to date on his new music.</li>
@@ -43,8 +43,21 @@
 			<b><u>Interaction Flow</u></b>
 		</header>
 
-		<p>
-
-		</p>
+		<ol>
+			<li class="user">Dylan logs onto SoundCloud on his MacBook</li>
+			<li class="site">SoundCloud displays Dylans music stream</li>
+			<li class="user">Dylan navigates to the "Upload" tab</li>
+			<li class="site">SoundCloud opens the page and gives him the option to upload his music</li>
+			<li class="user">Dylan finds the files then loads them to SoundCloud</li>
+			<li class="site">SoundCloud starts uploading the files and asks him to fill out the
+				information about his music while he waits</li>
+			<li class="user">Dylan then inputs the information about the track (title, artist, genre, artwork, etc.)</li>
+			<li class="site">SoundCloud puts the track on his personal profile along with all of the info</li>
+			<li class="user">Dylan shares his music on groups filled with people with interest in similar music to his</li>
+			<li class="site">SoundCloud alerts users in the groups that new music has been posted and keeps track of who plays it</li>
+			<li class="user">Dylan sees the number of plays climbing in his statistics</li>
+			<li class="user">Dylan is stoked</li>
+		</ol>
 	</body>
+
 </html>
