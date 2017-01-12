93838a7c33e83b7f40b6669130d9af235a9d5f3e
Dante Conley <danteconley@icloud.com>
2017-01-12 08:53:11 -0700

    Added Title and User Persona

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<title>SoundCloud User Persona</title>
+	</head>
+
+
+	<body>
+
+		<header>
+			<b><u>User Persona</u></b>
+		</header><br>
+
+		<ul>
+			<li><b>Name:</b> Dylan</li>
+			<li><b>Age:</b> 23</li>
+			<li><b>Occupation:</b> Music Producer and DJ</li>
+			<li><b>Technology:</b> Macbook Pro</li>
+			<li><b>Needs:</b> Up and coming music producer using SoundCloud as a platform
+					to promote and share his music. He needs tools to see who is listening
+					and where they are listening from to better understand his audience.</li>
+			<li><b>Goals:</b> He wants to find his niche market and make it big as a DJ.
+				Once fame is achieved SoundCloud will still be utilized to interact with fans
+				and keep them up to date on his new music.</li>
+		</ul>
+
+
+	</body>
+</html>
