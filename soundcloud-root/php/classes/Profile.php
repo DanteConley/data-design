764174c617dfef836acdba78944396dbfc2b8369
Dante Conley <danteconley@icloud.com>
2017-01-19 10:36:51 -0700

    adderd to profile class

--- soundcloud-root/php/classes/Profile.php
+++ soundcloud-root/php/classes/Profile.php
@@ -11,8 +11,9 @@ require_once("Profile.php");
  * @author Dante Conley <danteconley@icloud.com>
  * @version 1.0
  **/
-class Profile implements \JsonSerializable {
-	use ValidateDate;
+class Profile //implements \JsonSerializable
+	//use ValidateDate;
+{
 	/**
 	 * id for this Profile; this is the primary key
 	 * @var int $profileId
@@ -20,24 +21,58 @@ class Profile implements \JsonSerializable {
 	private $profileId;
 	/**
 	 * User name of the profile
-	 * @var int $profileUser
+	 * @var string $profileUser
 	 **/
 	private $profileUser;
 	/**
 	 * Location of where the profile user lives
-	 * @var int $profileLocation
+	 * @var string $profileLocation
 	 **/
 	private $profileLocation;
 	/**
 	 * Contact information for the profile
-	 * @var $profileContact
+	 * @var string $profileContact
 	 **/
 	private $profileContact;
 	/**
 	 * Information about the user of the profile
-	 * @var $profileBio
+	 * @var string $profileBio
 	 **/
 	private $profileBio;
 
-
+	/**
+	 * Constructor for the Profile
+	 *
+	 * @param int|null $newProfileId id of this profile or null if a new Profile
+	 * @param string $newProfileUser username of the Profile
+	 * @param string $newProfileLocation location of the user of the Profile
+	 * @param string $newProfileContact contact information for the user of the Profile
+	 * @param string $newProfileBio bio of the user of the Profile
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 **/
+	public function _construct(int $newProfileId = null, string $newProfileUser, string $newProfileLocation, string $newProfileContact, string $newProfileBio) {
+		try {
+			$this->setProfileId($newProfileId);
+			$this->setProfileUser($newProfileUser);
+			$this->setProfileLocation($newProfileLocation);
+			$this->setProfileContact($newProfileContact);
+			$this->setProfileBio($newProfileBio);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			// rethrow the exception to the caller
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $range) {
+			// rethrow the exception to the caller
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		} catch(\TypeError $typeError) {
+			//rethrow the exception to the caller
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		}catch(\Exception $exception) {
+			// rethrow the exception to the caller
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
+	/**
 }
